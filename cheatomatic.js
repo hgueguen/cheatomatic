d578ce39aaf64b1cc33aa52b2481c6684104b839
Harry GUEGUEN <hgueguen@e2r4p22.42.fr>
2016-02-12 15:02:12 +0100

    Yo

--- cheatomatic.js
+++ cheatomatic.js
@@ -1,21 +1,41 @@
+// ====		LA DATE
+var START_DATE = "Mon 15.2"
+var	RETRY_TIME = 10000
+
+
+// ====		INITIALISAGE
 var casper = require('casper').create({
-	//verbose: true,
+	verbose: true,
 	logLevel: 'debug'
-})
+});
+casper.options.waitTimeout = 10000;
 var mouse = require("mouse").create(casper);
 
-casper.start('https://stuart.staffomaticapp.com');
+// ====		LES FONCTIONS
+function	ft_register() {
+	casper.log("NEW WEEK PUBLISHED, LEGO !", 'info');
+	
+}
 
-casper.then(function() {
+function	ft_retry_later() {
+	casper.log("New week is not published yet. Ten seconds before retry", 'info');
+	casper.click('a.navbar-brand');
+	casper.wait(RETRY_TIME);
+	casper.waitForSelector('i.fa.fa-calendar.fa-lg.fa-fw', function() {
+		casper.click("i.fa.fa-calendar.fa-lg.fa-fw");
+		casper.waitForText(START_DATE, ft_register(), ft_retry_later());
+	});
+}
+
+// ====		LANCEMENT DES HOSTILITES
+casper.start('https://stuart.staffomaticapp.com', function() {
 	casper.waitForSelector('button.btn.btn-default', function() {
 		casper.sendKeys("input[name='user[email]']", "harry.gueguen@gmail.com");
 		casper.sendKeys("input[name='user[password]']", "42310102Ss");
 		casper.click("#login-form button");
 		casper.waitForSelector('i.fa.fa-calendar.fa-lg.fa-fw', function() {
 			casper.click("i.fa.fa-calendar.fa-lg.fa-fw");
-			casper.waitForSelector('div.schedule-item-container.active', function() {
-				//casper.debugHTML();
-			});
+			casper.waitForText(START_DATE, ft_register(), ft_retry_later());
 		});
 	});
 });
